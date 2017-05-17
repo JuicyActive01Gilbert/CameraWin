58a3ed8504ec4693bb26495996ded45e85889bcb
x925472958l <810242728@qq.com>
2017-05-17 16:12:25 +0800

    Add files via upload

---
+++ camerawin.cpp
@@ -0,0 +1,238 @@
+#include "camerawin.h"
+#include <QCamera>
+#include <QCameraViewfinderSettings>
+#include <QCameraImageCapture>
+#include <QImage>
+#include <QTimer>
+#include <QDir>
+#include <QDateTime>
+
+CamareWin::CamareWin(QWidget *parent) :
+    QWidget(parent)
+{
+	m_pCamera = NULL;
+	m_pCapture = NULL;
+	m_pTimer = NULL;
+	m_dirScreen = HOR_SCRREN;
+    setupUi(this);
+
+	initialize();
+}
+
+CamareWin::~CamareWin()
+{
+
+}
+
+void CamareWin::setScreenDir(SCREEN_DIR dir /* = HOR_SCRREN */)
+{
+	m_dirScreen = dir;
+}
+
+void CamareWin::startCamera()
+{
+	if (!m_pTimer->isActive())
+	{
+		m_pTimer->start(TIMER_CYCLE);
+	}
+	if (m_pCamera->isAvailable())
+	{
+		m_pCamera->start();
+	}
+}
+
+void CamareWin::stopCamera()
+{
+	if (!m_pTimer->isActive() )
+	{
+		m_pTimer->stop();
+	}
+
+	if (!m_pCamera->isAvailable())
+	{
+		m_pCamera->stop();
+	}
+
+}
+
+void CamareWin::takePhoto()
+{
+	stopCamera();
+	QString path = QDateTime::currentDateTime().toString("Pic_yyyy_MM_dd_hh_mm_ss") + ".png";
+	m_strPath = SAVE_PATH + path;
+	m_image.save(m_strPath);
+}
+
+void CamareWin::resTakePhoto()
+{
+	startCamera();
+	clearImagePath();
+	removeImage();
+}
+
+void CamareWin::clearImagePath()
+{
+	m_strPath.clear();
+}
+
+void CamareWin::removeImage()
+{
+	if (m_strPath.isEmpty())
+	{
+		return;
+	}
+	QDir dir;
+	if (!dir.exists(m_strPath))
+	{
+		return;
+	}
+
+	dir.remove(m_strPath);
+}
+
+QString CamareWin::getImagePath()
+{
+	return m_strPath;
+}
+
+void CamareWin::setCameraSettings(CAMERA_PARAM settings)
+{
+	if (m_pCamera == NULL)
+	{
+		return;
+	}
+
+	QCameraViewfinderSettings set;
+	set.setMinimumFrameRate(settings.nMinFrame);
+	set.setMaximumFrameRate(settings.nMaxFrame);
+	set.setResolution(settings.sizeResolution);
+	m_pCamera->setViewfinderSettings(set);
+}
+
+QList<CAMERA_PARAM> CamareWin::getCameraSupportSettings()
+{
+	QList<CAMERA_PARAM> lstSettings;
+
+	lstSettings.clear();
+	if (m_pCamera == NULL)
+	{
+		return lstSettings;
+	}
+
+	QList<QCameraViewfinderSettings> lstSize = m_pCamera->supportedViewfinderSettings();
+	for (int i = 0; i < lstSize.count(); i++)
+	{
+		CAMERA_PARAM setting;
+		setting.nMaxFrame = lstSize.at(i).maximumFrameRate();
+		setting.nMinFrame = lstSize.at(i).minimumFrameRate();
+		setting.sizeResolution = lstSize.at(i).resolution();
+		lstSettings.append(setting);
+	}
+	return lstSettings;
+}
+
+void CamareWin::initialize()
+{
+	m_pCamera = new QCamera(this);
+	m_pCapture = new QCameraImageCapture(m_pCamera);
+	connect(m_pCapture, SIGNAL(imageCaptured(int, QImage)), this, SLOT(slotDisplayImage(int, QImage)));
+	m_pCamera->setCaptureMode(QCamera::CaptureStillImage);
+	m_pCamera->start();
+
+	//定时器
+	m_pTimer = new QTimer(this);
+	connect(m_pTimer, SIGNAL(timeout()), this, SLOT(slotTimeOut()));
+	if (!m_pCamera->isAvailable())
+	{
+		emit signalCameraOpen(false);
+		return;
+	}
+	//判断缓存目录是否存在，不存在则创建
+	QDir dir;
+	if (!dir.exists(TEMP_DIR))
+	{
+		dir.mkpath(TEMP_DIR);
+	}
+	if (!dir.exists(QStringLiteral(SAVE_PATH)))
+	{
+		dir.mkpath(QStringLiteral(SAVE_PATH));
+	}
+
+	emit signalCameraOpen(true);
+}
+
+QImage CamareWin::cutImage(QImage src)
+{
+	QImage resImage;
+	int nOldW = 0;
+	int nOldH = 0;
+	int nScaledW = 0;//缩放的宽度
+	int nScaledH = 0;//缩放的高度
+	int nWinW = 0;
+	int nWinH = 0;
+	int x = 0;
+	int y = 0;
+
+	nWinW = this->width();
+	nWinH = this->height();
+	nOldW = src.width();
+	nOldH = src.height();
+
+	switch (m_dirScreen)
+	{
+		//竖屏
+	case VER_SCRREN:
+	{
+		if (nOldW/nOldH*1.0 > nWinW/nWinH*1.0)
+		{
+			nScaledH = nWinH;
+			nScaledW = nWinH / nOldH * nOldW;//得到能覆盖整个窗口的照片的大小
+			x = (nScaledW - nWinW) / 2;
+		}
+		else
+		{
+			nScaledW = nWinW;
+			nScaledH = nWinW / nOldW * nOldH;//得到能覆盖整个窗口的照片的大小
+			y = (nScaledH - nWinH) / 2;
+		}
+	}
+		break;
+		//横屏
+	case HOR_SCRREN:
+	{
+		if (nOldH / nOldW*1.0 > nWinH / nWinW*1.0)
+		{
+			nScaledH = nWinH;
+			nScaledW = nWinH / nOldH * nOldW;//得到能覆盖整个窗口的照片的大小
+			x = (nScaledW - nWinW) / 2;
+		}
+		else
+		{
+			nScaledW = nWinW;
+			nScaledH = nWinW / nOldW * nOldH;//得到能覆盖整个窗口的照片的大小
+			y = (nScaledH - nWinH) / 2;
+		}
+	}
+		break;
+	default:
+		break;
+	}
+	src = src.scaled(nScaledW, nScaledH);
+	resImage = src.copy(x, y, nWinW, nWinH);
+	return resImage;
+}
+
+void CamareWin::slotDisplayImage(int, QImage img)
+{
+	img = img.mirrored(true, false);
+	img = cutImage(img);//img.scaled(m_pLabImage->size());//, Qt::KeepAspectRatio);
+	m_image = img;
+	m_pLabImage->setPixmap(QPixmap::fromImage(img));
+}
+
+void CamareWin::slotTimeOut()
+{
+	QDir temDir(TEMP_DIR);//path中不能包含中文
+	QString strDir = temDir.absolutePath();
+	m_pCapture->capture(strDir + TEMP_FILE_NAME);
+}
---
+++ camerawin.h
@@ -0,0 +1,84 @@
+#ifndef CAMAREWIN_H
+#define CAMAREWIN_H
+
+#include "ui_camerawin.h"
+#include <QWidget>
+#include <QSize>
+
+#define SAVE_PATH "./照片/" //保存路径
+#define TEMP_DIR "./temp/" //缓存路径
+#define TEMP_FILE_NAME "temp.png" //缓存名称
+#define TIMER_CYCLE 24 //定时器周期
+
+class QImage;
+class QCamera;
+class QCameraViewfinderSettings;
+class QCameraImageCapture;
+class QTimer;
+
+//屏幕方向
+typedef enum ScreenDirationEnum
+{
+	HOR_SCRREN = 0,
+	VER_SCRREN = 1
+} SCREEN_DIR;
+
+//摄像头参数
+typedef struct CameraParameterStruct
+{
+	int nMinFrame;//最小帧率
+	int nMaxFrame;//最大帧率
+	QSize sizeResolution;//分辨率
+
+	CameraParameterStruct()
+	{
+		clear();
+	}
+
+	void clear()
+	{
+		nMinFrame = 5;
+		nMaxFrame = 5;
+		sizeResolution = QSize(640,480);
+	}
+	
+} CAMERA_PARAM;
+
+
+class CamareWin : public QWidget,public Ui_CameraWin
+{
+    Q_OBJECT
+
+public:
+    explicit CamareWin(QWidget *parent = 0);
+    ~CamareWin();
+
+	void setScreenDir(SCREEN_DIR dir = HOR_SCRREN);//设置屏幕方向,默认为横屏
+    void startCamera();//开启摄像头
+	void stopCamera();//停止摄像头
+	void takePhoto();//照相
+	void resTakePhoto();//重照
+	void clearImagePath();//清除路径
+	void removeImage();//清除图片
+	QString getImagePath();//获取图片路径
+	void setCameraSettings(CAMERA_PARAM settings);//设置摄像头参数
+	QList<CAMERA_PARAM> getCameraSupportSettings();//获取摄像头所有参数
+signals:
+	void signalCameraOpen(bool);//摄像头打开信号
+protected:
+	void initialize();//初始化
+	QImage cutImage(QImage src);//裁剪图片，从横屏截成竖屏
+protected slots:
+	void slotDisplayImage(int,QImage);//显示每张图片
+	void slotTimeOut();//计时器结束
+private:
+	QCamera* m_pCamera;
+	QCameraImageCapture* m_pCapture;
+	QTimer* m_pTimer;//定时器
+	SCREEN_DIR m_dirScreen;//屏幕方向
+	QString m_strPath;//图片路径
+	QImage m_image;//图片
+
+};
+
+#endif // CAMAREWIN_H
